e3d5b7b2f9f8d9a40dfbf73b0473c9e0a03b7f13
ideletemyself <bmckimmons@me.com>
2024-01-06 00:21:41 -0600

    made name/title on navbar route back to home as well.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
+      <Link href="/" className="px-3 py-2 rounded hover:bg-gray-700">
         <h1 className="text-xl font-semibold">Brandon McKimmons Portfolio</h1>
+      </Link>
         <div>
           <Link href="/" className="px-3 py-2 rounded hover:bg-gray-700">
             Home

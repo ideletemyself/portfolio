1d37dbaee8a203c053a96f3d2aad379b6a07b31e
ideletemyself <bmckimmons@me.com>
2024-01-08 16:29:21 -0600

    added hero.

---
+++ app/components/Hero.tsx
@@ -0,0 +1,25 @@
+// components/Hero.tsx
+
+import React from 'react';
+
+interface HeroProps {
+  title: string;
+  subtitle: string;
+  backgroundImage: string;
+}
+
+const Hero: React.FC<HeroProps> = ({ title, subtitle, backgroundImage }) => {
+  return (
+    <div className="relative bg-cover bg-center h-96 sm:h-96 md:h-96 lg:h-96 xl:h-screen 2xl:h-screen text-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative flex items-center justify-center h-full">
+        <div className="text-center">
+          <h1 className="text-4xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-8xl font-bold">{title}</h1>
+          <p className="text-2xl sm:text-2xl md:text-4xl lg:text-4xl xl:text-4xl ">{subtitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Hero from './components/Hero';
 
 const Home: React.FC = () => {
   const [astroImage, setAstroImage] = useState<string>('');
@@ -37,25 +38,19 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-col bg-white min-h-screen py-2 px-4 sm:px-20">
-      <main className="flex flex-col flex-auto w-full text-center">
-        <div className="flex flex-col xl:pl-44">
-          <h1 className="text-4xl sm:text-6xl text-left text-purple-900 py-4">
-            Photography
-          </h1>
-          <h1 className="text-4xl sm:text-6xl text-left text-purple-900 py-4">
-            Programming
-          </h1>
-          <h1 className="text-4xl sm:text-6xl text-left text-purple-900 py-4">
-            And Producing Music
-          </h1>
-        </div>
-        <div className="flex flex-col sm:flex-row justify-center">
+      <Hero 
+        title="Brandon McKimmons" 
+        subtitle="Photography, Programming & Producing Music" 
+        backgroundImage="/images/photos/DSC01612.webp" 
+      />  
+      <main className="flex flex-col flex-auto w-full text-center py-4">
+        <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row justify-center">
           
         <Link href="/astrophotography" className="max-w-sm bg-white rounded overflow-hidden shadow-2xl mb-4 mr-2">
             <div className="flex flex-col max-w-4xl mt-6 sm:w-full">
             <div className="px-6 py-4">
-            {astroImage && <Image className="w-full rounded" src={astroImage} alt="Astrophotography" width={720} height={400} />}
-                <h3 className="font-bold sm:text-lg md:text-base lg:text-2xl xl:text-3xl text-purple-700 mb-2">Astrophotography</h3>
+            {astroImage && <Image className="w-full rounded" src={astroImage} alt="Astrophotography" width={720} height={400} priority />}
+                <h3 className="font-bold text-2xl sm:text-2xl md:text-base lg:text-2xl xl:text-3xl text-purple-700 mb-2">Astrophotography</h3>
                 <article className="sm:text-lg md:text-base lg:text-2xl xl:text-2xl text-purple-700 text-opacity-75">
                   My collections of astrophotography.
                 </article>
@@ -66,7 +61,7 @@ const Home: React.FC = () => {
           <Link href="/nature" className="max-w-sm bg-white rounded overflow-hidden shadow-2xl mb-4 mr-2">
             <div className="flex flex-wrap max-w-4xl mt-6 sm:w-full">
             <div className="px-6 py-4">
-            {natureImage && <Image className="w-full rounded" src={natureImage} alt="Animals" width={720} height={400} />}
+            {natureImage && <Image className="w-full rounded" src={natureImage} alt="Animals" width={720} height={400} priority />}
                 <h3 className="font-bold text-2xl sm:text-3xl text-purple-700 mb-2">Nature</h3>
                 <article className="text-lg sm:text-xl text-purple-700 text-opacity-75">
                   My collections of nature photos.
@@ -78,7 +73,7 @@ const Home: React.FC = () => {
           <Link href="/animals" className="max-w-sm bg-white rounded overflow-hidden shadow-2xl mb-4 mr-2">
             <div className="flex flex-wrap max-w-4xl mt-6 sm:w-full">
             <div className="px-6 py-4">
-            {animalImage && <Image className="w-full rounded" src={animalImage} alt="Animals" width={720} height={400} />}
+            {animalImage && <Image className="w-full rounded" src={animalImage} alt="Animals" width={720} height={400} priority />}
                 <h3 className="font-bold text-2xl sm:text-3xl text-purple-700 mb-2">Animals</h3>
                 <article className="text-lg sm:text-xl text-purple-700 text-opacity-75">
                   My collections of animal photos.

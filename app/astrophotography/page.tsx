f006a4c3a6779228cc4c07a3b7f4a5153d92d2db
ideletemyself <bmckimmons@me.com>
2024-01-08 20:02:41 -0600

    fullscreen image on click on astrophotography page.

--- app/astrophotography/page.tsx
+++ app/astrophotography/page.tsx
@@ -25,22 +25,36 @@ const Astrophotography: React.FC = () => {
     }
   };
 
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="flex bg-white flex-col items-center justify-center min-h-screen py-2 px-4 sm:px-20">
-      <main className="flex flex-col items-center justify-center flex-1 w-full text-center sm:text-left">
-        <h1 className="text-4xl sm:text-6xl py-4 text-left text-purple-900">Astrophotography</h1>
-        <div className="max-w-4xl bg-white rounded overflow-hidden shadow-2xl">
-          {images.length > 0 && <Image className="flex h-auto px-3 py-4 space-x-4" src={images[currentImageIndex]}
-                  width={900}
-                  height={600}
-                  alt="Slide" />}
+    <>
+      {isModalOpen && (
+        <div onClick={toggleModal} className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black">
+          <img src={images[currentImageIndex]} alt="Slide" className="max-w-full max-h-full" />
         </div>
-        <div className="flex px-3 py-4 space-x-4">
-          <button className="bg-purple-900 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full" onClick={prevImage}>Previous</button>
-          <button className="bg-purple-900 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full" onClick={nextImage}>Next</button>
-        </div>
-      </main>
-    </div>
+      )}
+      <div className="flex bg-white flex-col items-center justify-center min-h-screen py-2 px-4 sm:px-20">
+        <main className="flex flex-col items-center justify-center flex-1 w-full text-center sm:text-left">
+          <h1 className="text-4xl sm:text-6xl py-4 text-left text-purple-900">Astrophotography</h1>
+          <div className="max-w-4xl bg-white rounded overflow-hidden shadow-2xl">
+            {images.length > 0 && <Image className="flex h-auto px-3 py-4 space-x-4" src={images[currentImageIndex]}
+                    width={900}
+                    height={600}
+                    alt="Slide"
+                    onClick={toggleModal} />}
+          </div>
+          <div className="flex px-3 py-4 space-x-4">
+            <button className="bg-purple-900 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full" onClick={prevImage}>Previous</button>
+            <button className="bg-purple-900 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full" onClick={nextImage}>Next</button>
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 

b7c01a7d97205d78c775c373ec1cf50b2051e05f
ideletemyself <bmckimmons@me.com>
2024-01-09 19:50:57 -0600

    updated each photo page to be more responsive.

--- app/astrophotography/page.tsx
+++ app/astrophotography/page.tsx
@@ -35,18 +35,26 @@ const Astrophotography: React.FC = () => {
     <>
       {isModalOpen && (
         <div onClick={toggleModal} className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black">
-          <Image src={images[currentImageIndex]} alt="Slide" className="max-w-full max-h-full" width={900} height={600} />
+          <div className='w-screen sm:max-w-sm md:max-w-xl lg:max-w-lg xl:max-w-lg 2xl:max-w-lg'>
+          <Image src={images[currentImageIndex]} alt="Slide" 
+          width={7000}
+          height={7000} 
+          priority />
+          </div>
         </div>
       )}
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 px-4 sm:px-20">
-        <main className="flex flex-col items-center justify-center flex-1 w-full text-center sm:text-left">
-          <h1 className="text-4xl sm:text-6xl py-4 text-left text-purple-900">Astrophotography</h1>
+      <div className="items-center justify-center min-h-screen px-4 sm:px-20">
+        <main className="flex flex-col items-center justify-center text-center">
+          <h1 className="text-4xl sm:text-6xl py-4 text-purple-900">Astrophotography</h1>
           <div className="max-w-4xl bg-white rounded overflow-hidden shadow-2xl">
+          <div className='w-full sm:max-w-sm md:max-w-md lg:max-w-md xl:max-w-md 2xl:max-w-md'>
             {images.length > 0 && <Image className="flex h-auto px-3 py-4 space-x-4" src={images[currentImageIndex]}
-                    width={900}
-                    height={600}
+                    width={7000}
+                    height={7000}
                     alt="Slide"
-                    onClick={toggleModal} />}
+                    onClick={toggleModal} 
+                    priority />}
+          </div>
           </div>
           <div className="flex px-3 py-4 space-x-4">
             <button className="bg-purple-900 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full" onClick={prevImage}>Previous</button>

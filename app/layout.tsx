6849baeb01fe61bba83c76456c77ecd56d6a6078
ideletemyself <bmckimmons@me.com>
2024-01-12 14:48:52 -0600

    trying to get rid of -webkit-text-size-adjust warning.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
       <body className={inter.className}>
           <Navbar />
           {children}

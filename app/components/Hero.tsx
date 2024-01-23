b4aa6371d19ff8938bd97111ebe5aec08a6f4b50
ideletemyself <bmckimmons@me.com>
2024-01-22 20:35:46 -0600

    converted hero component to Image and uses CDN

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,7 @@
 // components/Hero.tsx
 
 import React from 'react';
+import Image from 'next/image';
 
 interface HeroProps {
   title: string;
@@ -10,15 +11,30 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ title, subtitle, backgroundImage }) => {
   return (
-    <div className="relative bg-cover bg-center h-96 sm:h-96 md:h-96 lg:h-96 xl:h-screen 2xl:h-screen text-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative flex items-center justify-center h-full">
-        <div className="text-center">
-          <h1 className="subpixel-antialiased text-4xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-8xl font-bold">{title}</h1>
-          <p className="subpixel-antialiased text-2xl sm:text-2xl md:text-4xl lg:text-4xl xl:text-4xl ">{subtitle}</p>
+    <>
+      <div className="relative h-1/2 w-full">
+        <Image className="object-cover w-full -z-1"
+          src={backgroundImage}
+          sizes="(min-width: 1280px) 1256px, (min-width: 1040px) 744px, (min-width: 780px) 648px, calc(100vw - 24px)"
+          style={{
+            objectFit: 'contain'
+          }}
+          width={1900}
+          height={900}
+          alt="Slide"
+          placeholder="blur"
+          blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII='}
+        />
+        <div className="absolute inset-0 bg-black opacity-50">
+          <div className="relative flex items-center justify-center h-full">
+            <div className="text-center text-white">
+              <h1 className="subpixel-antialiased text-2xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-8xl font-bold">{title}</h1>
+              <p className="subpixel-antialiased text-xl sm:text-2xl md:text-4xl lg:text-4xl xl:text-4xl ">{subtitle}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/
-  });
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
     images: {
-        domains: ['brandonmckimmons-nextjs-563476088.imgix.net'],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'brandonmckimmons-nextjs-563476088.imgix.net',
+        }, ],
     },
 }
 

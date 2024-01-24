aa7389d14971624a7bb3d27d8a63907c05ddbe64
ideletemyself <bmckimmons@me.com>
2024-01-24 14:15:38 -0600

    changed to local og:image for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Viewport } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import OGimage from "./images/OG-SunSky-1200x630.jpg";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -31,7 +32,21 @@ export const metadata: Metadata = {
     description: 'Brandon McKimmons Portfolio, talking about and showing off my photography, programming and music production. I also have a blog where I talk about various related topics.',
     url: 'https://brandonmckimmons.com/',
     siteName: 'brandonmckimmons.com',
-    images: ['https://static.frontendmasters.com/assets/fm/med/home/fm-opengraph-v3.jpg'],
+    images: [
+      {
+        url: OGimage,
+        type: 'image/jpeg',
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: 'https://brandonmckimmons-nextjs-563476088.imgix.net/OG-SunSky-600x315.jpg',
+        type: 'image/jpeg',
+        width: 600,
+        height: 315,
+        alt: 'Brandon McKimmons Portfolio, talking about and showing off my photography, programming and music production. I also have a blog where I talk about various related topics.',
+      },
+    ],
     locale: 'en_US',
     type: 'website',
   },

dcd456ad92f993515ea74fd0f78287e197a539b2
ideletemyself <bmckimmons@me.com>
2024-01-23 22:28:05 -0600

    removed 2nd og:image to see if smaller will work now in FB messenger

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,17 +39,11 @@ export const metadata: Metadata = {
         height: 630,
       },
       {
-        url: 'https://brandonmckimmons-nextjs-563476088.imgix.net/OG-SunSky-600x315.jpg',
-        type: 'image/jpeg',
-        width: 600,
-        height: 315,
-        alt: 'Brandon McKimmons Portfolio, talking about and showing off my photography, programming and music production. I also have a blog where I talk about various related topics.',
-      },
-      {
         url: 'https://brandonmckimmons-nextjs-563476088.imgix.net/OG-SunSky-220x114.jpg',
         type: 'image/jpeg',
         width: 200,
         height: 104,
+        alt: 'Brandon McKimmons Portfolio, talking about and showing off my photography, programming and music production. I also have a blog where I talk about various related topics.',
       },
     ],
     locale: 'en_US',

57c44cf33143e3c807e6d7ec0c05ce6cd9db5100
ideletemyself <bmckimmons@me.com>
2024-02-01 21:16:12 -0600

    going back to no blur placeholder or blurhash, client side and server side stuff clashing too much

--- app/astrophotography/page.tsx
+++ app/astrophotography/page.tsx
@@ -1,90 +1,68 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import ImgixClient from '@imgix/js-core';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import { imgixLoader } from '../components/ImgixLoader';
 import {
   ArrowLeftCircleIcon,
   ArrowRightCircleIcon,
 } from '@heroicons/react/24/outline';
 
 const Astrophotography: React.FC = () => {
-  const [images, setImages] = useState([
+  const images = [
     {
       src: '/Astrophotography/Blue_Orion_Shot1',
       alt: 'The Orion constellation with a blue hue, 1st photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Blue_Starry_Night_Shot1',
       alt: 'A random shot of a section of the night sky with a blue hue',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Purple_Orion_Shot1',
       alt: 'The Orion constellation with a purple hue, 1st photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Purple_Orion_Shot2',
       alt: 'The Orion constellation with a purple hue, 2nd photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Purple_Orion_Shot3',
       alt: 'The Orion constellation with a purple hue, 3rd photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Starry_Night_Sky_with_Clouds_Shot1',
       alt: 'A colorful photo of a cloudy night sky, 1st photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Starry_Night_Sky_with_Clouds_Shot2',
       alt: 'A colorful photo of a cloudy night sky, 2nd photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Starry_Night_Sky_with_Clouds_Shot3',
       alt: 'A colorful photo of a cloudy night sky, 3rd photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Starry_Night_Sky_with_Clouds_Shot4',
       alt: 'A colorful photo of a cloudy night sky, 4th photo',
-      blurHash: '',
     },
     {
       src: '/Astrophotography/Night_Streetlight_Gate',
       alt: 'A photo of a street light behind a gate with the night sky in the background',
-      blurHash: '',
     },
-  ]);
+  ];
 
-  useEffect(() => {
-    const loadImages = async () => {
-      try {
-        const loadedImages = await imgixLoader(images);
-        console.log('Loaded images:', loadedImages); // Log the loaded images
+  const imgixClient = new ImgixClient({
+    domain: 'brandonmckimmons-nextjs-563476088.imgix.net',
+  });
 
-        const processedImages = loadedImages.map((image, index) => {
-          console.log(`Processing image ${index}:`, image); // Log each image
-          return {
-            src: image.url, // map 'url' to 'src'
-            alt: image.alt, // include 'alt' in the object
-            blurHash: image.blurHash, // include 'blurHash' in the object
-          };
-        });
-
-        setImages(processedImages);
-      } catch (error) {
-        console.error('Error loading images:', error); // Log any errors
-      }
-    };
-
-    loadImages();
-  }, []);
+  const imgUrl = (image: { src: string; alt: string }) =>
+    imgixClient.buildURL(`${image.src}.webp`, {
+      fit: 'fill', // fill mode
+      auto: 'format,compress', // auto format and compress
+      lossless: 1,
+      // ... other Imgix parameters
+    });
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
@@ -118,7 +96,7 @@ const Astrophotography: React.FC = () => {
           <div className='w-full'>
             <Image
               className='w-full max-h-svh mx-auto'
-              src={images[currentImageIndex].src}
+              src={imgUrl(images[currentImageIndex])}
               alt={images[currentImageIndex].alt}
               sizes='(min-width: 1280px) 1256px, (min-width: 1040px) 744px, (min-width: 780px) 648px, calc(100vw - 24px)'
               style={{
@@ -127,7 +105,9 @@ const Astrophotography: React.FC = () => {
               width={7028}
               height={4688}
               placeholder='blur'
-              blurDataURL={`data:image/png;base64,${images[currentImageIndex].blurHash}`}
+              blurDataURL={
+                'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII='
+              }
             />
           </div>
         </div>
@@ -161,7 +141,7 @@ const Astrophotography: React.FC = () => {
             <div>
               <Image
                 className='object-cover max-h-svh max-w-min px-3 py-3 z-10'
-                src={images[currentImageIndex].src}
+                src={imgUrl(images[currentImageIndex])}
                 alt={images[currentImageIndex].alt}
                 sizes='(min-width: 1280px) 1256px, (min-width: 1040px) 744px, (min-width: 780px) 648px, calc(100vw - 24px)'
                 style={{
@@ -171,7 +151,9 @@ const Astrophotography: React.FC = () => {
                 height={4688}
                 onClick={toggleModal}
                 placeholder='blur'
-                blurDataURL={`data:image/png;base64,${images[currentImageIndex].blurHash}`}
+                blurDataURL={
+                  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII='
+                }
                 priority
               />
             </div>
@@ -196,6 +178,6 @@ const Astrophotography: React.FC = () => {
       </div>
     </>
   );
-};
+}
 
 export default Astrophotography;
